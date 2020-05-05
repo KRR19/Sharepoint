e5c9bbfd2b49aff55c6fa2c3f061fcc45679be2a
Роман Казаров <kazarovroman@gmail.com>
2020-05-05 08:40:29 +0300

    create list of subsites

---
+++ CreateListOfSubSites/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace CreateListOfSubSites
+{
+    class Program
+    {
+        private static string ParentSiteUrl = "http://win-k1qikcue0no:85/";
+        static void Main(string[] args)
+        {
+            GetListOfSubSites(ParentSiteUrl);
+            Console.ReadKey();
+        }
+
+        private static void GetListOfSubSites(string url)
+        {
+            try
+            {
+                ClientContext context = new ClientContext(url);
+                Web web = context.Web;
+                context.Load(web, w => w.Webs, w => w.Title);
+                context.ExecuteQuery();
+                foreach (Web childWeb in web.Webs)
+                {
+                    string childWebPathUrl = ParentSiteUrl + childWeb.ServerRelativeUrl;
+                    GetListOfSubSites(childWebPathUrl);
+                    Console.WriteLine(childWebPathUrl);
+                    Console.WriteLine(childWeb.Title);
+                    Console.WriteLine("--------------------------");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
